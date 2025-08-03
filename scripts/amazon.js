b74f0bd417103d855a4c9e8f07717fb55aa11cba
MOKSH MANRAL <moksh@MOKSHs-MacBook-Air.local>
2025-08-04 01:36:04 +0530

    Cart Items Functionality Added

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,40 +1,11 @@
-const products = [{
-  img: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  ratings: {
-    stars: 4.5,
-    count: 87
-  },
-  priceCents: 1090
-},
-{
-  img: 'images/products/intermediate-composite-basketball.jpg',
-  name: 'Intermediate Size Basketball',
-  ratings: {
-    stars: 4.0,
-    count: 127
-  },
-  priceCents: 2095
-},
-{
-  img: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-  ratings: {
-    stars: 4.5,
-    count: 56
-  },
-  priceCents: 799
-},
-];
-
 let productsHTML = '';
 
 products.forEach((product) => {
-  productsHTML += `
+  productsHTML +=`
     <div class="product-container">
             <div class="product-image-container">
               <img class="product-image"
-                src="${product.img}">
+                src="${product.image}">
             </div>
 
             <div class="product-name limit-text-to-2-lines">
@@ -43,9 +14,9 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
               <img class="product-rating-stars"
-                src="images/ratings/rating-${product.ratings.stars * 10}.png">
+                src="images/ratings/rating-${product.rating.stars * 10}.png">
               <div class="product-rating-count link-primary">
-                ${product.ratings.count}
+                ${product.rating.count}
               </div>
             </div>
 
@@ -75,13 +46,37 @@ products.forEach((product) => {
               Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${product.id}">
               Add to Cart
             </button>
-          </div>;
-  `
+          </div>
+`
   
 });
-console.log(productsHTML);
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+  button.addEventListener('click',()=>{
+    const productId = button.dataset.productId;
+
+    let matchingItem;
+
+    cart.forEach((item)=>{
+      if(productId === item.productId){
+        matchingItem = item;
+      }
+    });
+
+    if(matchingItem){
+      matchingItem.quantity+=1;
+    }else{
+      cart.push({
+        productId:productId,
+        quantity:1
+      });
+    }
+
+    console.log(cart);
+  })
+})
