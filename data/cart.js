961fc11c791c18f4873625b64b8c95742ec4c130
MokshManral <mokshmanral628@gmail.com>
2025-08-06 12:55:28 +0530

    make delivery options interactive

--- data/cart.js
+++ data/cart.js
@@ -46,3 +46,17 @@ export function removeFromCart(productId){
 
   saveToStorge();
 }
+ 
+export function updateDeliveryOption(productId,deliveryOptionId){
+  let matchingItem;
+  
+  cart.forEach((cartItem)=>{
+      if(productId === cartItem.productId){
+        matchingItem = cartItem;
+      }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    saveToStorge();
+}
